085fca3f6b7017255e3950bf353f630c2d9512ce
doldrums <doldrums.317@gmail.com>
2022-11-09 22:00:07 +0300

    deps

---
+++ lib/connection/connection_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConnectionScreen extends StatelessWidget {
+  const ConnectionScreen({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Тут подключаемся'),
+      ),
+    );
+  }
+}
